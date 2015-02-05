1ae968aa5d026af621bb082f8312a95d51777d9c
Xavier Castel <xavier.castel@gmail.com>
2015-02-05 21:59:13 +0100

    Deezer track selection
    ngTable selection
    Send only selected tracks to last.fm
    Refactor controllers as View Models, avoid scope
    Styling

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,18 +3,21 @@
 // Controllers
 var deezerImportControllers = angular.module('deezerImportControllers', ['ngTable']);
 
-deezerImportControllers.controller('DeezerController', ['$scope', 'DeezerSearch', 'DeezerHistory', '$filter', 'ngTableParams', 
-	function ($scope, DeezerSearch, DeezerHistory, $filter, ngTableParams) {
-		$scope.userName = 'titixies';
+deezerImportControllers.controller('DeezerController', ['$scope', '$filter', 'ngTableParams', 'DeezerSearch', 'DeezerHistory', 
+	function ($scope, $filter, ngTableParams, DeezerSearch, DeezerHistory) {
+		var vm = this;
+	
+		vm.userName = 'titixies';
 		// get it from http://developers.deezer.com/api/explorer
-		$scope.accessToken = 'frkEeL2Zzr54d29d523e896DitEZwXA54d29d523e8cdwnV8lRM';
-		$scope.deezerUser = {};
-		var deezerTracks = [];
-		$scope.pagesLoaded = 0;
+		vm.accessToken = 'fruSZ4cSK754d3d71464242ItO0enRO54d3d7146427aBofpBrA';
+		vm.deezerUser = {};
+		
+		vm.deezerTracks = [];
+		vm.pagesLoaded = 0;
 		
 		// cf. http://stackoverflow.com/questions/19674125/how-do-i-filter-a-row-based-on-any-column-data-with-single-textbox/19676463#19676463
-		$scope.trackFilter = '';
-		$scope.$watch('trackFilter', function () {
+		vm.trackFilter = '';
+		$scope.$watch('vm.trackFilter', function () {
 			$scope.tableParams.reload();
 		});
 		
@@ -22,113 +25,127 @@ deezerImportControllers.controller('DeezerController', ['$scope', 'DeezerSearch'
 			page: 1,
 			count: 25,
 			sorting: {
-				timestamp: 'asc'
+				timestamp: 'desc'
 			}
 		}, {
-			total: deezerTracks.length,
+			total: 0,
 			getData: function($defer, params) {
 				// use build-in angular filter
-				var filteredData = $scope.trackFilter ? $filter('filter')(deezerTracks, $scope.trackFilter) : deezerTracks;
+				var filteredData = vm.trackFilter ? $filter('filter')(vm.deezerTracks, vm.trackFilter) : vm.deezerTracks;
 				var orderedData = params.sorting() ? $filter('orderBy')(filteredData, params.orderBy()) : filteredData;
 				$defer.resolve(orderedData.slice((params.page() - 1) * params.count(), params.page() * params.count()));
 			}
 		})
 
-		$scope.searchUser = function() {
-			console.log('Searching Deezer for user ' + $scope.userName);
+		vm.searchUser = function() {
+			console.log('Searching Deezer for user ' + vm.userName);
 			
 			// Call deezer search
-			DeezerSearch.get({q: $scope.userName}, function(data) {
-				$scope.deezerUser = data.data[0];
-				console.log('Found user ' + $scope.deezerUser.id);
+			DeezerSearch.get({q: vm.userName}, function(data) {
+				vm.deezerUser = data.data[0];
+				console.log('Found user ' + vm.deezerUser.id);
 
 				// retrieve history
-				$scope.getUserHistory();
+				vm.getUserHistory();
 			});
 		}
 
-		$scope.getUserHistory = function() {
-			console.log('Getting history for user ' + $scope.deezerUser.name);
-			var indexToLoad = 50 * $scope.pagesLoaded;
+		vm.getUserHistory = function() {
+			console.log('Getting history for user ' + vm.deezerUser.name);
+			var indexToLoad = 50 * vm.pagesLoaded;
 
-			DeezerHistory.get({userId: $scope.deezerUser.id, access_token: $scope.accessToken, index: indexToLoad}, function(data) {
+			DeezerHistory.get({userId: vm.deezerUser.id, access_token: vm.accessToken, index: indexToLoad}, function(data) {
 				if (!data.error) {
-					deezerTracks = deezerTracks.concat(data.data);
+					vm.deezerTracks = vm.deezerTracks.concat(data.data);
 					$scope.tableParams.reload();
-					$scope.pagesLoaded++;
+					vm.pagesLoaded++;
 				} else {
 					console.error('Deezer history returned error : ' + data.error.message);
 				}
 			});
+		}
+		
+		vm.selectAllTracks = function() {
+			vm.deezerTracks.forEach(function(track) {
+				track.selected = !track.selected;
+			});
+		}
+		
+		vm.selectedTracksNb = function() {
+			return vm.deezerTracks.filter(function(tr) {
+				return tr.selected
+			}).length;
 		}
 	}
 ]);
 
 deezerImportControllers.controller('LastfmController', ['$scope', 'LastfmService', 
 	function ($scope, LastfmService) {
-		$scope.userName = 'titix29';
+		var vm = this;
+	
+		vm.userName = 'titix29';
 		// cf. http://www.lastfm.fr/api/accounts
-		$scope.api_key = '0d464d63b340f345585d8321599a91c4';
-		var secret = 'FILL-ME';
-		$scope.accessToken = 'fe54022c4ddb5bd926784db98bebdda5';
-		$scope.lastfmUser = {};
-		$scope.session = {};
+		vm.api_key = '0d464d63b340f345585d8321599a91c4';
+		var secret = '';
+		vm.accessToken = '';
+		vm.lastfmUser = {};
+		vm.session = {};
 
-		$scope.searchUser = function() {
-			console.log('Searching Lastfm for user ' + $scope.userName);
+		vm.searchUser = function() {
+			console.log('Searching Lastfm for user ' + vm.userName);
 			
 			// Call last.fm search
-			LastfmService.get($scope.userName, $scope.api_key)
+			LastfmService.get(vm.userName, vm.api_key)
 				.success(function(data) {
-					$scope.lastfmUser = data.user;
-					console.log('Found user ' + $scope.lastfmUser.name);
+					vm.lastfmUser = data.user;
+					console.log('Found user ' + vm.lastfmUser.name);
 					
-					$scope.getToken();
+					vm.getToken();
 				});
 		}
 		
-		$scope.getToken = function() {
-			LastfmService.getToken($scope.api_key, secret, function(data) {
+		vm.getToken = function() {
+			LastfmService.getToken(vm.api_key, secret, function(data) {
 				console.log('getToken returned : ' + data.token);
-				$scope.accessToken = data.token;
+				vm.accessToken = data.token;
 				
 				// needed because we are in a sub-function ?
 				$scope.$apply();
 			});
 		}
 		
-		$scope.getSession = function() {
-			LastfmService.getSession($scope.accessToken, $scope.api_key, secret, function(data) {
+		vm.getSession = function() {
+			LastfmService.getSession(vm.accessToken, vm.api_key, secret, function(data) {
 				console.log('getSession returned : ' + data.session);
-				$scope.session = data.session;
+				vm.session = data.session;
 				$scope.$apply();
 			});
 		}
 		
-		$scope.sendTracks = function() {
+		vm.sendTracks = function() {
+			// cf. http://stackoverflow.com/questions/25417162/how-do-i-inject-a-controller-into-another-controller-in-angularjs
 			var deezerDiv = document.getElementById('deezerDiv');
 			var deezerScope = angular.element(deezerDiv).scope();
 		
-			// convert deezer to lastfm tracks
-			var lastfmTracks = deezerScope.tableParams.data.map(function(track) {
-				return {
-					artist: track.artist.name,
-					track: track.title,
-					timestamp: track.timestamp,
-					album: track.album.title,
-					chosenByUser: 1
-				};
-			});
+			// convert selected deezer tracks to lastfm tracks
+			var lastfmTracks = deezerScope.vm.deezerTracks
+				.filter(function(track) { return track.selected; })
+				.map(function(track) {
+					return {
+						artist: track.artist.name,
+						track: track.title,
+						timestamp: track.timestamp,
+						album: track.album.title,
+						chosenByUser: 1
+					};
+				});
 			
 			// last.fm supports 50 elements batches
 			// cf. http://stackoverflow.com/questions/11318680/split-array-into-chunks-of-n-length for splicing
 			var batchSize = 50;
 			while(lastfmTracks.length > 0) {
-				LastfmService.sendTracks(lastfmTracks.splice(0, batchSize), $scope.session, $scope.api_key, secret);
+				LastfmService.sendTracks(lastfmTracks.splice(0, batchSize), vm.session, vm.api_key, secret);
 			}
-			
-			// DEBUG : send last track only
-			// LastfmService.sendTracks(lastfmTracks[0], $scope.session, $scope.api_key, secret);
 		}
 	}
 ]);
